a5d97ca7f4f028d3d8d83b9641aa6eeab2c9f1c4
Sam Houston <shouston3@me.com>
2017-04-07 16:04:27 +0100

    commit after running 'mix new . --app markdown_cms'

---
+++ lib/markdown_cms.ex
@@ -0,0 +1,18 @@
+defmodule MarkdownCms do
+  @moduledoc """
+  Documentation for MarkdownCms.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MarkdownCms.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/markdown_cms_test.exs
@@ -0,0 +1,8 @@
+defmodule MarkdownCmsTest do
+  use ExUnit.Case
+  doctest MarkdownCms
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
